ec8edcdaf9f071797b488ef3551ad91d4c53e117
edimez14 <edimez14@gmail.com>
2024-09-30 15:16:25 -0500

    update file Request.api.js

--- client/src/api/Request.api.js
+++ client/src/api/Request.api.js
@@ -1,13 +1,13 @@
 /* This JavaScript code snippet is setting up an Axios instance called `usersApi` with a base URL of
-"http://localhost:8000/login/api/v1/". It then defines several functions to interact with an API for
+"/choreo-apis/loginandregister/backend/v1". It then defines several functions to interact with an API for
 user authentication and registration. */
 import axios from 'axios';
 
-/* The code snippet `const usersApi = axios.create({baseURL: "https://login-and-register-wrn2.onrender.com/api/v1/",})`
+/* The code snippet `const usersApi = axios.create({baseURL: "/choreo-apis/loginandregister/backend/v1",})`
 is creating an Axios instance named `usersApi`. This instance is configured with a base URL of
-"https://login-and-register-wrn2.onrender.com/api/v1/". */
+"/choreo-apis/loginandregister/backend/v1". */
 const usersApi = axios.create({
-	baseURL: "https://localhost:8000/login/api/v1/",
+	baseURL: "/choreo-apis/loginandregister/backend/v1",
 })
 
 /**
